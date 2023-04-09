87041c06e0131bb805359c070da8e990bac32887
Ginjinator <elliserik69@gmail.com>
2023-04-09 12:10:50 -0400

    Add some parent objects

---
+++ Assets/Animal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    //Light, medium, heavy, etc
+    public ClassType playerClassType;
+
+    public int totalEnergy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
