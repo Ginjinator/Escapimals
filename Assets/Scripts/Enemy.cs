03268add1967c5205ef076a45227b8f898f71225
Ginjinator <elliserik69@gmail.com>
2023-04-30 15:49:43 -0400

    Change spawn rate and add random health for enemy spawn

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,16 +8,10 @@ public class Enemy : ScriptableObject
     public string enemyName;
 
     public Sprite artwork;
-    public int health;
+    public int baseHealth;
 
     //Attack and damamge or block amount
     public Sprite action;
     public int actionAmount;
 
-    public void Awake()
-    {
-        var randomHealth = Random.Range(0, 12);
-        //Try to set random stats at start
-        //health = randomHealth;
-    }
 }
